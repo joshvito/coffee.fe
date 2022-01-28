6c72a8aa301997a0a6ba9d484e656c03afde4860
Joshua Vito <jvito@anthology.com>
2022-01-28 15:25:52 -0500

    adds dismiss prop to nav selection;

--- src/app/components/brew-methods/brew-methods.component.ts
+++ src/app/components/brew-methods/brew-methods.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-brew-methods',
   template: `
+  <div class="p-1">
     <p>
       brew-methods works!
     </p>
+  </div>
+    
   `,
   styles: [
   ]
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { LayoutView } from 'src/app/models/layout.model';
@@ -10,20 +10,20 @@ import * as fromReducer from '../../state/reducers';
   template: `
     <nav class="navbar navbar-light bg-light fixed-top">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Offcanvas navbar</a>
+        <a class="navbar-brand" href="#">Coffee Friends</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Coffee Friends</h5>
             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li class="nav-item" *ngFor="let view of [LayoutView.Ratings, LayoutView.Beans, LayoutView.Methods]">
                 <a class="nav-link" [class.active]="(currentLayoutView$ | async) === view"
-                aria-current="page" href="#" (click)="onViewChange(view)">{{LayoutView[view]}}</a>
+                aria-current="page" data-bs-dismiss="offcanvas" (click)="onViewChange(view)">{{LayoutView[view]}}</a>
               </li>
             </ul>
           </div>
