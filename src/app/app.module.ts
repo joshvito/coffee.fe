1b7c35ab7b57ed0e50e058c539355ff62fb72fcc
Joshua Vito <jvito@anthology.com>
2022-01-28 13:55:44 -0500

    inits some components; adds models;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { BrewMethodsComponent } from './components/brew-methods/brew-methods.component';
+import { BeansComponent } from './components/beans/beans.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    BrewMethodsComponent,
+    BeansComponent
   ],
   imports: [
     BrowserModule
