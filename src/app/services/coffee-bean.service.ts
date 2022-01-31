256f0a76897772d49ab85c28467d2c6fe8144af6
Joshua Vito <jvito@anthology.com>
2022-01-31 14:05:33 -0500

    adds a couple services;

--- src/app/services/coffee-bean.service.ts
+++ src/app/services/coffee-bean.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ICoffeeBean } from '../models/bean.model';
 import { IPageResult } from '../models/common.model';
 
