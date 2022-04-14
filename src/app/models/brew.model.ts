2273daf6db356eb8b5a56d1371319690e51510c6
Joshua Vito <jvito@anthology.com>
2022-04-14 16:26:15 -0400

    updates models and services for api changes;

---
+++ src/app/models/brew.model.ts
@@ -0,0 +1,23 @@
+import { INumberBooleanMap, IPageRequest, ITimestamps } from './common.model';
+
+export enum Grind {
+  Fine, MediumFine, Medium, MediumCoarse, Coarse
+}
+
+export interface IBrew extends ITimestamps, ICreateBrew {
+    id: number;
+    user_id: number;
+}
+
+export interface ICreateBrew {
+    bean_id: number;
+    method_id: number;
+    grams: number;
+    grind?: Grind;
+    notes: string;
+}
+
+export interface IFilterBrew extends IPageRequest {
+  methods: INumberBooleanMap,
+  beans: INumberBooleanMap,
+}
--- src/app/services/rating.service.ts
+++ src/app/services/rating.service.ts
@@ -2,8 +2,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
-import { IBrewRatings, ICreateBrewRating, IFilterBrewRatings } from '../models/brew-ratings.model';
-import { IPageResult } from '../models/common.model';
+import { IBrewRatings, ICreateBrewRating } from '../models/brew-ratings.model';
+import { IPageRequest, IPageResult, paramMapper } from '../models/common.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,24 +12,8 @@ export class RatingService {
 
   constructor(private http: HttpClient) { }
 
-  getMany(filters: Partial<IFilterBrewRatings> = {}): Observable<IPageResult<IBrewRatings>> {
-    let params = new HttpParams();
-
-    if (filters.beans && Object.keys(filters.beans).length) {
-      params =  Object.keys(filters.beans).reduce((accum, id) => {
-        if(!filters.beans![Number(id)]) return accum;
-        return accum.append('beans[]', id);
-      }, params);
-    }
-
-    if (filters.methods && Object.keys(filters.methods).length) {
-      params =  Object.keys(filters.methods).reduce((accum, id) => {
-        if(!filters.methods![Number(id)]) return accum;
-        return accum.append('methods[]', id);
-      }, params);
-    }
-
-    return this.http.get<IPageResult<IBrewRatings>>(`${environment.apiUrl}ratings`, { params });
+  getMany(query: Partial<IPageRequest> = {}): Observable<IPageResult<IBrewRatings>> {
+    return this.http.get<IPageResult<IBrewRatings>>(`${environment.apiUrl}ratings`, { params: paramMapper(query) });
   }
 
   create(params: ICreateBrewRating): Observable<IBrewRatings> {
