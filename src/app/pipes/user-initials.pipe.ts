9cc91e9472f029af73f3247a69755cec84f17bbe
David Meyers <dmeyers@anthology.com>
2022-04-22 11:07:38 -0400

    make it a one lineer

--- src/app/pipes/user-initials.pipe.ts
+++ src/app/pipes/user-initials.pipe.ts
@@ -6,10 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class UserInitialsPipe implements PipeTransform {
 
   transform(username: string): string {
-    let initials = '';
     return username.split(' ').reduce((a, v) => {
       return a += v.substring(0,1)
-    }, initials);
+    }, '');
   }
 
 }
