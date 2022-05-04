47524af8b878e595f952c56e3f353807079acfb5
David Meyers <davidm0783@gmail.com>
2022-05-04 15:56:21 -0400

    updates homegrown star comp

--- src/app/components/stars/stars.component.ts
+++ src/app/components/stars/stars.component.ts
@@ -4,22 +4,33 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'app-stars',
   template: `
     <div>
-      <i class="fa fa-solid fa-star fs-7" aria-hidden="true"></i>
-      <i class="fa fa-solid fa-star fs-7" aria-hidden="true"></i>
-      <i class="fa fa-solid fa-star fs-7" aria-hidden="true"></i>
-      <i class="fa fa-solid fa-star fs-7" aria-hidden="true"></i>
-      <i class="fa fa-solid fa-star fs-7" aria-hidden="true"></i>
+      <i class="fa fa-solid fa-star fs-7 filled" aria-hidden="true" *ngFor="let i of numFilledStars"></i>
+      <i class="fa fa-solid fa-star fs-7 unfilled" aria-hidden="true" *ngFor="let i of numUnFilledStars"></i>
     </div>
   `,
-  styles: [
-  ]
+  styles: [`
+    .unfilled {
+      color: red;
+    }
+    .filled {
+      color: brown;
+    }
+  `]
 })
 export class StarsComponent implements OnInit {
   @Input() rating = 0;
 
+  maxStars = 5;
+  numFilledStars: number[] | null = null;
+  numUnFilledStars: number[] | null = null;
+
   constructor() { }
 
   ngOnInit(): void {
+    const r = this.rating > this.maxStars ? this.maxStars : Math.ceil(this.rating);
+
+    this.numFilledStars = Array(r).fill(null).map((x,i) => i);
+    this.numUnFilledStars = Array(this.maxStars - r).fill(null).map((x,i) => i);
   }
 
 }
