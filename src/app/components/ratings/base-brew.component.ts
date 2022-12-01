0d62deb0599bd3eddebabc2353e01c55872c44f8
Joshua Vito <jvito@anthology.com>
2022-12-01 13:01:25 -0500

    minor change to text formatting; CF-4

--- src/app/components/ratings/base-brew.component.ts
+++ src/app/components/ratings/base-brew.component.ts
@@ -4,7 +4,11 @@ export class BaseBrewComponent {
   constructor() {}
 
   getTitle(bean: ICoffeeBean): string {
-    return `${bean.brand} | ${bean.name ? bean.name : bean.origin } (${ Roast[bean.roast] } Roast)`
+    return `${bean.brand} ${bean.name ? bean.name : bean.origin } ${ Roast[bean.roast] } Roast`
+  }
+
+  getLabel(bean: ICoffeeBean): string {
+    return `[${bean.brand}] ${bean.name ? bean.name : bean.origin } (${ Roast[bean.roast] } Roast)`
   }
 
 }
