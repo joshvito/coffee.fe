b9bf40aaa86757ff091c1b62d8d4cb623991159f
David Meyers <dmeyers@anthology.com>
2022-12-07 14:41:54 -0500

    Adds form value sub with console.log

--- src/app/components/ratings/new-rating.component.ts
+++ src/app/components/ratings/new-rating.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { select, Store } from '@ngrx/store';
 import { filter } from 'rxjs/operators';
-import { OptionalRatingDescriptions, OptionalRatingKeys } from 'src/app/models/rating.model';
+import { OptionalRatingDescriptions, OptionalRatingKeys, optionalRatingKeysType } from 'src/app/models/rating.model';
 import { selectors, State } from 'src/app/state/reducers';
 
+type formKeys = 'brew_id' | 'rating' | 'notes' | optionalRatingKeysType;
+type IForm = {
+  [key in formKeys]: FormControl<string|null>
+}
 @Component({
   selector: 'app-new-rating',
   templateUrl: 'rating-modal.html',
   styles: []
 })
 export class NewRatingComponent implements OnInit {
-  form: UntypedFormGroup;
+  form: FormGroup;
   optionalKeys = OptionalRatingKeys;
   optionalDescriptions = OptionalRatingDescriptions;
 
   constructor(
     private store: Store<State>,
-    fb: UntypedFormBuilder,
+    fb: FormBuilder,
     public activeModal: NgbActiveModal
   ) {
     const optionalFields = this.optionalKeys.reduce((accum, key) => {
@@ -29,19 +35,29 @@ export class NewRatingComponent implements OnInit {
     }, {});
 
     this.form = fb.group({
-      'brew_id': fb.control(null, [Validators.required]),
-      'rating': fb.control(null, [Validators.required, Validators.max(5), Validators.min(1)]),
-      'notes': fb.control('', [Validators.max(255)]),
+      'brew_id': fb.control<string|null>(null, [Validators.required]),
+      'rating': fb.control<string|null>(null, [Validators.required, Validators.max(5), Validators.min(1)]),
+      'notes': fb.control<string|null>('', [Validators.max(255)]),
       ...optionalFields
     });
   }
 
   ngOnInit(): void {
+    this.form.valueChanges
+      .subscribe((fv) => {
+        console.log(fv);
+        // sum with other optionalField values that are not me
+        // parseInt their values
+        // divide by length of optionalFields
+        // set rating value to division answer.
+      });
+
+
     this.store.pipe(
       select(selectors['brew'].getSelectedBrew),
       filter(r => !!r)
     ).subscribe(b => {
-      if(!!b) {this.form.patchValue({'brew_id': b.id})}
+      if(!!b) {this.form.patchValue({'brew_id': b.id.toString()})}
     });
   }
 
